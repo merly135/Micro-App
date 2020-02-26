7cb3da5fc717791ae57d6acf355c222f84185500
Merly V Maldonado <mm28503@srcs.loc>
2020-02-26 14:44:50 -0800

    grf

--- app.js
+++ app.js
@@ -1,19 +1,40 @@
 var img = document.createElement("IMG");
 
-let scissorsPic = document.getElementById("scissors")
-let rockPic = document.getElementById("rock")
-let paperPic = document.getElementById("paper")
+var scissorsPic = document.getElementById("scissors");
 
-scissorsPic.addEventListener("click", () => {
-    var imgs = ["rock.jpg", "scissors.jpg", "paper.jpg"]
+function scissorsClick() {
+    "use strict";
+    var imgs = ["rock.jpg", "scissors.jpg", "paper.jpg"];
+    var random = Math.floor(Math.random() * 3);
 
 
+    img.setAttribute("src", imgs[random]);
+    document.body.appendChild(img);
 }
 
-rockPic.addEventListener("click", () => {
-    var imgs = ["rock.jpg", "scissors.jpg", "paper.jpg"]
+scissorsPic.addEventListener("click", scissorsClick());
+
+
+
+
+var rockPic = document.getElementById("rock");
+
+function rockClick() {
+    "use strict";
+    var imgs = ["rock.jpg", "scissors.jpg", "paper.jpg"];
 }
 
-paperPic.addEventListener("click", () => {
-    var imgs = ["rock.jpg", "scissors.jpg", "paper.jpg"]
+rockPic.addEventListener("click", rockClick());
+
+
+
+
+
+var paperPic = document.getElementById("paper");
+
+function paperClick() {
+    "use strict";
+    var imgs = ["rock.jpg", "scissors.jpg", "paper.jpg"];
 }
+
+rockPic.addEventListener("click", paperClick());
