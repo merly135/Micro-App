2e53e4bdf068d27101d57e2caf2316ad75a38e98
Merly V Maldonado <mm28503@srcs.loc>
2020-02-26 13:50:21 -0800

    update

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 var img = document.createElement("IMG");
-var img2 = document.createElement("IMG");
 
-function roshambo() {
-    var imgs = ["https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.123rf.com%2Fphoto_23262904_hand-sign-of-rock-paper-scissors-game-isolated-vector-on-white-background.html&psig=AOvVaw0ePh3qoxs3IjIitx7yYd2y&ust=1582654163220000&source=images&cd=vfe&ved=0CAMQjB1qFwoTCIixn-_k6ucCFQAAAAAdAAAAABAZ","https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.123rf.com%2Fphoto_23262897_hand-sign-of-rock-paper-scissors-game-isolated-vector-on-white-background.html&psig=AOvVaw0ePh3qoxs3IjIitx7yYd2y&ust=1582654163220000&source=images&cd=vfe&ved=0CAMQjB1qFwoTCIixn-_k6ucCFQAAAAAdAAAAABAe","https://www.google.com/imgres?imgurl=https%3A%2F%2Fpreviews.123rf.com%2Fimages%2Fjulinzy%2Fjulinzy1310%2Fjulinzy131000181%2F23262905-hand-sign-of-rock-paper-scissors-game-isolated-vector-on-white-background.jpg&imgrefurl=https%3A%2F%2Fwww.123rf.com%2Fphoto_23262905_hand-sign-of-rock-paper-scissors-game-isolated-vector-on-white-background.html&tbnid=OmCfklVlrkdf0M&vet=12ahUKEwjitPCq5OrnAhWD6J4KHeEACRcQMygCegUIARDaAQ..i&docid=0LjGNiGE9TrDMM&w=1300&h=1300&q=rock%20papers%20scissors%20hand%20sign&hl=en&safe=strict&ved=2ahUKEwjitPCq5OrnAhWD6J4KHeEACRcQMygCegUIARDaAQ"]
+let scissorsPic = document.getElementById("scissors")
+let rockPic = document.getElementById("rock")
+let paperPic = document.getElementById("paper")
+
+scissorsPic.addEventListener("click", () => {
+    var imgs = ["rock.jpg", "scissors.jpg", "paper.jpg"]
 
 
+}
+
+rockPic.addEventListener("click", () => {
+    var imgs = ["rock.jpg", "scissors.jpg", "paper.jpg"]
+}
+
+paperPic.addEventListener("click", () => {
+    var imgs = ["rock.jpg", "scissors.jpg", "paper.jpg"]
 }
